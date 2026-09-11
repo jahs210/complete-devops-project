289ddaf41e2a2d9844eb432246c29d7f25a4f598
jahs210 <torresjulian.210@gmail.com>
2026-09-11 18:46:11 -0400

    removin 0from python

--- terraform-configs/app.py
+++ terraform-configs/app.py
@@ -9,6 +9,4 @@ def get_curent_time():
     return f"the current time: {current_time}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0.0",port=8080)
-
-    
+    app.run(host="0.0.0.0", port=8080)
